7545086e0a8ec993d860b7c136d15fece6e50986
Vishwanathms <vishwanath.murthy@gmail.com>
2019-08-12 10:37:56 +0530

    Update app1.py

--- Catalog-Service/app1.py
+++ Catalog-Service/app1.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from redis import Redis
 import os
 import socket
 
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route("/")
 def hello():   
     html = "<h3>Hello APP1{name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" 
     return html.format(name=os.getenv("NAME", "APP1 - world"), hostname=socket.gethostname())
+    count = redis.incr('hits')
+    return 'Hello World! I have been seen {} times.\n'.format(count)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
