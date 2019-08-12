bfc64afde296047064f2f0f43307a1b3ddfdfc7a
Vishwanathms <vishwanath.murthy@gmail.com>
2019-08-12 11:06:23 +0530

    Update app1.py

--- Catalog-Service/app1.py
+++ Catalog-Service/app1.py
@@ -8,11 +8,10 @@ redis = Redis(host='redis', port=6379)
 
 @app.route("/")
 def hello():   
-    html = "<h3>Hello APP1{name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" 
-    return html.format(name=os.getenv("NAME", "APP1 - world"), hostname=socket.gethostname())
-    count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    html = "<h3>Hello APP1 {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+	   "<b>Hello World! I have been seen</b> {count}<br/> <b>times.\n</b>"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), count = redis.incr('hits'))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
